b80f66635140524c758cc3b5acb3edcf26dd6dce
rechall418 <liangruiqi92304@gmail.com>
2024-06-29 15:02:31 +0800

    6.29-15:02

--- src/App.vue
+++ src/App.vue
@@ -4,32 +4,13 @@ import { RouterLink, RouterView } from 'vue-router';
 import { useMouse } from './utils/hooks';
 
 // 定义初始类型
-const type = ref('Computed');
-const message = ref('你好');
-
-// 定义问候函数
-function greet(event) {
-  alert(`Hello ${type.value}!`);
-  // `event` 是 DOM 原生事件
-  if (event) {
-    alert(message.value);
-  }
-}
+const type = ref('');
 
 // 定义导航按钮数据
 const navItems = ref([
-  { name: 'Computed', route: '/computed' },
   { name: 'List', route: '/list' },
   { name: 'Hello', route: '/hello' },
-  { name: 'Form1', route: '/form1' },
-  { name: 'Life', route: '/life' },
-  { name: 'MouseTracker', route: '/mousetracker' }
 ]);
-
-const { x, y } = useMouse();
-// watch([x, y], ([newX, newY]) => {
-//   console.log(`x is ${newX} and y is ${newY}`);
-// }, { immediate: true });
 </script>
 
 <template>
@@ -44,28 +25,7 @@ const { x, y } = useMouse();
         >
           <RouterLink :to="item.route">{{ item.name.toLowerCase() }}</RouterLink>
         </button>
-      <!-- <button :class="{ active: type === 'Computed' }" @click="type = 'Computed'">
-        <RouterLink to="/computed">computed</RouterLink>
-      </button>
-      <button :class="{ active: type === 'List' }" @click="type = 'List'">
-        <RouterLink to="/list">list</RouterLink>
-      </button>
-      <button :class="{ active: type === 'Hello' }" @click="type = 'Hello'">
-        <RouterLink to="/hello">hello</RouterLink>
-      </button>
-      <button :class="{ active: type === 'Form1' }" @click="type = 'Form1'">
-        <RouterLink to="/form1">form1</RouterLink>
-      </button>
-      <button :class="{ active: type === 'Life' }" @click="type = 'Life'">
-        <RouterLink to="/life">life</RouterLink>
-      </button>
-      <button :class="{ active: type === 'MouseTracker' }" @click="type = 'MouseTracker'">
-        <RouterLink to="/life">MouseTracker</RouterLink>
-      </button> -->
     </nav>
-
-    
-
     </div>
 
     <router-view></router-view>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Computed from '../views/computed.vue';
 import List from '../views/list.vue';
 import Hello from '../components/HelloWorld.vue';
-import Form1 from '../views/from.vue';
-import Life from '../views/life.vue';
-import MouseTracker from '../views/MouseTracker.vue'; 
 import Home from '../views/home.vue';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/computed', component: Computed },
   { path: '/list', component: List },
   { path: '/hello', component: Hello },
-  { path: '/form1', component: Form1 },
-  { path: '/life', component: Life },
-  { path: '/mousetracker', component: MouseTracker } 
 ];
 
 const router = createRouter({
